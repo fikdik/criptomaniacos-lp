d3ce058af5587cf06500bca77c0cd99eea72b95d
Pedro Moreno <nennogabriel@gmail.com>
2022-01-31 16:50:49 -0300

    refactory: gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,60 @@
-module.exports = {
-  siteMetadata: {
-      title: `criptomaniacos-lp`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-netlify-cms", {
-    resolve: 'gatsby-plugin-google-analytics',
+// const path = require('path');
+const siteMetadata = require('./src/content/settings/siteMetadata.json');
+const publicKeys = require('./src/content/settings/publicKeys.json');
+
+let plugins = [`gatsby-plugin-netlify`, 'gatsby-plugin-netlify-cms'];
+
+if (process.env.NODE !== 'development') {
+  plugins.push({
+    resolve: `gatsby-plugin-sitemap`,
     options: {
-      "trackingId": "xxxx"
-    }
-  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+      exclude: [`/*/obrigado`],
+    },
+  });
+  if (!!publicKeys.gtag) {
+    plugins.push({
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: publicKeys.gtag,
+      },
+    });
+  }
+  if (!!publicKeys.tawkto) {
+    plugins.push({
+      resolve: `gatsby-plugin-tawk.to`,
+      options: {
+        tawkId: publicKeys.tawkto,
+      },
+    });
+  }
+}
+
+plugins.push(
+  'gatsby-plugin-react-helmet',
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
+      icon: 'src/images/icon.png',
+    },
+  },
+  'gatsby-plugin-mdx',
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      name: 'pages',
+      path: './src/pages/',
     },
-    __key: "pages"
-  }]
+    __key: 'pages',
+  }
+);
+
+console.log(
+  `**************************************************  ${JSON.stringify(
+    plugins
+  )}   ******************************************* ${process.env.NODE} *******`
+);
+
+module.exports = {
+  siteMetadata,
+  plugins,
 };
