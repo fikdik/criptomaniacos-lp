1bba9eb1f819c1f88885d17c7f7d511a945a05c1
Pedro Moreno <nennogabriel@gmail.com>
2022-01-31 15:41:49 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+      title: `criptomaniacos-lp`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "xxxx"
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
